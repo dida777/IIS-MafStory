2b0059f6c184da884f83b6e4a3ca8479db10a7bc
Diana Barnova <dianabarnova@gmail.com>
2018-11-30 20:51:42 +0100

    in work
    added new functions

--- kontrolery/ToDoListKontroler.php
+++ kontrolery/ToDoListKontroler.php
@@ -17,6 +17,11 @@ class ToDoListKontroler extends Kontroler {
 				$this->data["ulohy"] = Uloha::getClenUlohy($_SESSION["rodne_cislo"]);
 			}
 
+			// prihlaseny je DON
+			if ($_SESSION["typ"] == 1) {
+				if (isset($parametry[0]) && ($parametry[0] == "new_work"))
+					$this->new_work();
+			}
 
 			if (isset($parametry[0])) {
 				switch ($parametry[0]) {
@@ -32,13 +37,44 @@ class ToDoListKontroler extends Kontroler {
 			$this->pohled = 'chyba';
 	}
 
-	public function zacatUlohu() {
-		Uloha::updateCasZaciatku($_POST["specificke_meno"], $_POST["cas_zaciatku"]);
-		$this->presmeruj("todolist");
+	public function new_work() {
+		// iba pre prihlasenych
+		if (isset($_SESSION["rodne_cislo"])) {
+			$this->pohled = 'new_work';
+			$this->data["success"] = "";
+			$this->hlavicka['titulek'] = 'Zadať novú úlohu';
+			$this->data["don"] = new Don($_SESSION["rodne_cislo"]);
+			if (!empty($_POST)){
+				try {
+					$success = Uloha::insertUloha($_POST);
+					if ($success != 0)
+						$this->data["success"] = "Nová úloha bola úspešne zadaná!";
+				} catch (Exception $e) {
+					if (strpos($e->getMessage(), 'Duplicate') !== false){
+					 	$this->data["success"] = "Špecifické meno úlohy už existuje.";
+					} else
+						$this->data["success"] = "Chyba pri zadávaní, skúste znovu.";
+				}
+			}						
+		} else
+			$this->pohled = 'chyba';
 	}
 
-	public function ukoncitUlohu() {		
-		Uloha::updateCasKonca($_POST["specificke_meno"], $_POST["cas_konca"], $_POST["uspesnost"], $_POST["komentar"]);
-		$this->presmeruj("todolist");
+	public function zacatUlohu() {
+		// iba pre prihlasenych
+		if (isset($_SESSION["rodne_cislo"])) {
+			Uloha::updateCasZaciatku($_POST["specificke_meno"], $_POST["cas_zaciatku"]);
+			$this->presmeruj("todolist");
+		} else
+			$this->pohled = 'chyba';
+	}
+
+	public function ukoncitUlohu() {	
+		// iba pre prihlasenych	
+		if (isset($_SESSION["rodne_cislo"])) {
+			Uloha::updateCasKonca($_POST["specificke_meno"], $_POST["cas_konca"], $_POST["uspesnost"], $_POST["komentar"]);
+			$this->presmeruj("todolist");
+		} else
+			$this->pohled = 'chyba';
 	}
 }

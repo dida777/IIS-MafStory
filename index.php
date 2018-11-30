9c90d5a59f2ac06685a12c7611f9627d809c4bfa
dida777 <34103458+dida777@users.noreply.github.com>
2018-11-30 11:56:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+// Nastavení interního kódování pro funkce pro práci s řetězci
+mb_internal_encoding("UTF-8");
+
+// Callback pro automatické načítání tříd controllerů a modelů
+function autoloadFunkce($trida)
+{
+	// Končí název třídy řetězcem "Kontroler" ?
+    if (preg_match('/Kontroler/', $trida))	
+        require("kontrolery/" . $trida . ".php");
+    else
+        require("modely/" . $trida . ".php");
+}
+
+// Registrace callbacku (Pod starým PHP 5.2 je nutné nahradit fcí __autoload())
+spl_autoload_register("autoloadFunkce");
+
+// Připojení k databázi
+Db::pripoj("127.0.0.1", "root", "", "mafia");
+
+// Vytvoření routeru a zpracování parametrů od uživatele z URL
+$smerovac = new SmerovacKontroler();
+$smerovac->zpracuj(array($_SERVER['REQUEST_URI']));
+
+// Vyrenderování šablony
+$smerovac->vypisPohled();
+
+//source www.itnetwork.cz

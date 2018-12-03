15d1da655754dc0f4f191943bbc2c00298c19edc
Diana Barnova <dianabarnova@gmail.com>
2018-12-03 17:26:01 +0100

    update

--- xbarno00.sql
+++ xbarno00.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hostiteľ: 127.0.0.1
--- Čas generovania: Ne 02.Dec 2018, 17:17
+-- Čas generovania: Po 03.Dec 2018, 17:25
 -- Verzia serveru: 10.1.37-MariaDB
 -- Verzia PHP: 7.2.12
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databáza: `mafia`
+-- Databáza: `xbarno00`
 --
 
 -- --------------------------------------------------------
@@ -39,8 +39,9 @@ CREATE TABLE `aliancia` (
 --
 
 INSERT INTO `aliancia` (`id_aliancie`, `nazov_aliancie`, `datum_vzniku`) VALUES
+(0, 'admin', '0000-00-00'),
 (1, 'Milovnici kolacikov', '2018-12-05'),
-(3, 'Smrteľné sesterstvo', '2018-12-02');
+(4, 'Smrteľné sesterstvo', '2018-12-14');
 
 -- --------------------------------------------------------
 
@@ -50,7 +51,7 @@ INSERT INTO `aliancia` (`id_aliancie`, `nazov_aliancie`, `datum_vzniku`) VALUES
 
 CREATE TABLE `clen` (
   `rodne_cislo` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-  `familia` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `familia` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `hodnost` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `pokrvna_vazba` varchar(3) COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
@@ -70,7 +71,10 @@ INSERT INTO `clen` (`rodne_cislo`, `familia`, `hodnost`, `pokrvna_vazba`) VALUES
 ('8854308782', 'Reynosa Cartel', 'uklizec', 'ne'),
 ('9054018094', 'Dianne Cartel', 'snajper', 'ano'),
 ('9306284130', 'Dianne Cartel', 'vymahac', 'ano'),
-('9707028463', 'Salieri Cartel', 'uklizec', 'ano');
+('9602157896', 'Monique Cartel', 'vymahac', 'ne'),
+('9707028463', 'Salieri Cartel', 'uklizec', 'ano'),
+('9901015678', 'Monique Cartel', 'sofer', 'ano'),
+('admin', 'admin', 'admin', '---');
 
 -- --------------------------------------------------------
 
@@ -93,9 +97,10 @@ CREATE TABLE `don` (
 INSERT INTO `don` (`rodne_cislo`, `nazov_familie`, `gps_uzemie`, `aliancia`, `zvolal_zraz`) VALUES
 ('450808538', 'Morello Cartel', 'N 49°12.16327, E 16°37.24875', NULL, NULL),
 ('6905317254', 'Salieri Cartel', 'N 49°13.59087, E 16°35.78202', NULL, NULL),
-('8660138509', 'Reynosa Cartel', 'N 26°3.88785, W 98°17.80297', 3, 2),
-('9754034862', 'Monique Cartel', 'N 36°0.84342, E 14°19.46250', 3, NULL),
-('9854032958', 'Dianne Cartel', 'N 48°34.63458, E 19°7.55460', 3, NULL);
+('8660138509', 'Reynosa Cartel', 'N 26°3.88785, W 98°17.80297', 4, 2),
+('9754034862', 'Monique Cartel', 'N 36°0.84342, E 14°19.46250', 4, 8),
+('9854032958', 'Dianne Cartel', 'N 48°34.63458, E 19°7.55460', 4, NULL),
+('admin', 'admin', 'admin', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -130,9 +135,12 @@ INSERT INTO `osoba` (`rodne_cislo`, `heslo`, `meno`, `priezvisko`, `vek`, `typ`)
 ('8854308782', '1234', 'Katherine', 'Alekminskaya', 30, 0),
 ('9054018094', '1234', 'Yevgeniya', 'Novakova', 28, 0),
 ('9306284130', '1234', 'Alois', 'Stula', 25, 0),
+('9602157896', '1234', 'Rudolf', 'Kučera', 22, 0),
 ('9707028463', '1234', 'Chuck', 'Norris', 21, 0),
 ('9754034862', '1234', 'Monika', 'Monique', 20, 1),
-('9854032958', '1234', 'Diana', 'Dianne', 19, 1);
+('9854032958', '1234', 'Diana', 'Dianne', 19, 1),
+('9901015678', '1234', 'Aladin', 'Horvath', 20, 0),
+('admin', 'admin', 'admin', 'admin', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -158,7 +166,9 @@ CREATE TABLE `uloha` (
 --
 
 INSERT INTO `uloha` (`specificke_meno`, `popis_cinnosti`, `vykonavatel`, `zadavatel_don`, `zadavatel_aliancia`, `gps_miesta`, `cas_zaciatku`, `cas_konca`, `uspesnost`, `komentar`) VALUES
+('admin', 'admin', 'admin', 'admin', 0, 'admin', '2018-12-01 23:00:00', '2018-12-01 23:00:00', 'admin', 'admin'),
 ('bumbac zalezitost', 'ciel: Brabry Forbon', '9707028463', '9754034862', NULL, 'N 36°0.84342, E 14°19.46250', '2018-11-30 15:55:16', '2018-11-29 22:00:00', 'ano', 'sadfd'),
+('na fasirky', 'rozmlatit', '0051010014', '9754034862', NULL, 'N 40°46.37197, W 73°58.51303', NULL, NULL, '', ''),
 ('ocista', 'podraz, potreba zbavit sa', '0051010014', '9854032958', NULL, 'N 26°3.88785, W 98°17.80297', '2018-11-30 15:50:11', '2018-11-29 23:59:00', 'ano', 'hihihi'),
 ('vyber vypalneho', 'Maly Denny meska s platbou', '9306284130', '8660138509', NULL, 'N 29°58.56720, W 90°7.16213', '1999-06-16 22:00:00', '1999-06-16 22:00:00', 'ano', 'dhdtjdjt'),
 ('zachranna sluzba', 'vazenie Bohunice, 2 ludia', '8854308782', '9854032958', NULL, 'N 49°10.46560, E 16°34.78125', '1985-05-16 22:00:00', '1985-05-16 22:00:00', 'ano', NULL),
@@ -180,6 +190,7 @@ CREATE TABLE `uzemie` (
 --
 
 INSERT INTO `uzemie` (`gps`, `hodnota`) VALUES
+('admin', 0),
 ('N 26°3.88785, W 98°17.80297', 50),
 ('N 29°58.56720, W 90°7.16213', 10),
 ('N 36°0.84342, E 14°19.46250', 100),
@@ -207,7 +218,9 @@ CREATE TABLE `zraz_donov` (
 --
 
 INSERT INTO `zraz_donov` (`id_zrazu`, `datum_cas`, `usporiadatel`, `gps_miesta`) VALUES
-(2, '2018-12-21 10:22:00', '8660138509', 'N 48°34.63458, E 19°7.55460');
+(0, '2018-12-03 14:00:54', 'admin', 'admin'),
+(2, '2018-12-21 10:22:00', '8660138509', 'N 48°34.63458, E 19°7.55460'),
+(8, '2018-12-13 23:59:00', '9754034862', 'N 29°58.56720, W 90°7.16213');
 
 --
 -- Kľúče pre exportované tabuľky
@@ -230,6 +243,7 @@ ALTER TABLE `clen`
 --
 ALTER TABLE `don`
   ADD PRIMARY KEY (`rodne_cislo`),
+  ADD UNIQUE KEY `nazov_familie` (`nazov_familie`),
   ADD KEY `gps_uzemie` (`gps_uzemie`,`aliancia`),
   ADD KEY `aliancia` (`aliancia`),
   ADD KEY `zvolal_zraz` (`zvolal_zraz`);
@@ -273,7 +287,7 @@ ALTER TABLE `zraz_donov`
 -- AUTO_INCREMENT pre tabuľku `aliancia`
 --
 ALTER TABLE `aliancia`
-  MODIFY `id_aliancie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_aliancie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pre tabuľku `zraz_donov`
